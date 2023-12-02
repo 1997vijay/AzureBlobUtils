518e8d21a396da78548f2c0560a9759f0e334986
1997vijay <1140vijaykumar@gmail.com>
2023-12-02 20:22:50 +0530

    Initial release

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+name='AzureBlobUtils',
+version='0.1',
+author="Vijay Kumar",
+author_email="vijay.kumar.1997@outlook.com",
+description="Package can be used to intrect with Azure Storage account. It can be use to download to list file,containers, upload file,download files",
+packages=['AzureBlobUtils'],
+install_requires=[
+        'azure-storage-blob',
+        'pandas',
+    ],
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+)
