93a9aec1b12a1e48b6a5d20568b9c082693a3187
1997vijay <1140vijaykumar@gmail.com>
2023-12-09 20:04:43 +0530

    Modified the code

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
-import setuptools
-setuptools.setup(
-name='AzureUtils',
-version='0.1',
-author="Vijay Kumar",
-author_email="vijay.kumar.1997@outlook.com",
-description="Package can be used to intrect with Azure Storage account. It can be use to download to list file,containers, upload file,download files",
-packages=['AzureUtils'],
-install_requires=[
-        'azure-storage-blob',
-        'pandas',
-    ],
-classifiers=[
-"Programming Language :: Python :: 3",
-"License :: OSI Approved :: MIT License",
-"Operating System :: OS Independent",
-],
-)
+# setup.py
+from setuptools import setup
+
+if __name__ == "__main__":
+  setup(packages = ['azure_strg_utils'])
